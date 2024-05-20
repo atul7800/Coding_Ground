8d5016e3e59715ddceb5b82736c057002b1295dd
atul <atulgupta.d796@gmil.com>
2024-05-20 14:55:15 +0530

    20-5-024

--- JS_By_Chai_aur_code/Project_Set_1/P3_Digital_clock/script.js
+++
@@ -1,7 +0,0 @@
-let timeBox = document.getElementById('clock');
-
-setInterval(function(){
-    let date = new Date();
-    let time = date.toLocaleTimeString();
-    timeBox.innerText = time;
-}, 1000);
